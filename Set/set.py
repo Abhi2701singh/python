848eea44992e9ae03087a46491ec18625beedd3d
ABHINAV SINGH <162627370+Abhi2701singh@users.noreply.github.com>
2025-03-06 22:48:16 +0530

    Add files via upload

---
+++ Set/set.py
@@ -0,0 +1,7 @@
+# set of integers
+my_set = {1, 2, 3, 4, 5}
+print(my_set)  # Output: {1, 2, 3, 4, 5}
+
+# set of mixed datatypes
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)  # Output: {1.0, 'Hello', (1, 2, 3)}
